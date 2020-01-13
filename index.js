7af40e3796a7fecf91906969f3c9b5a6e74e3b6d
Aximilli1212 <aximill1212@gmail.com>
2020-01-13 17:03:19 +0000

    Add filterlist method

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const { prisma } = require('./generated/prisma-client')
 // A `main` function so that we can use async/await
 async function main() {
     // Create a new user called `Alice`
-    const newUser = await prisma.createUser({ name: 'Alice' })
+    const newUser = await prisma.createUser({ name: 'Shaolinix' })
     console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
     // Read all users from the database and print them to the console
     const allUsers = await prisma.users()
-    console.log(allUsers)
+    // console.log(allUsers)
+
+    //Filter user list
+    const usersCalledAlice = await prisma.users({
+        where: {
+            name: 'Alice',
+        },
+    });
+    console.log(usersCalledAlice);
 }
+
+
 
 main().catch(e => console.error(e))
